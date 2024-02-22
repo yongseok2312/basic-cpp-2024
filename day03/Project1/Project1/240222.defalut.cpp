54b85e592efb9bdf50a331b96d49ac4a4dc1ad3a
yongseok2312 <lyos2312@naver.com>
2024-02-22 09:45:00 +0900

    수정

---
+++ day03/Project1/Project1/240222.defalut.cpp
@@ -0,0 +1,17 @@
+/*
+default
+*/
+/*
+#include <iostream>
+using namespace std;
+
+int Adder(int num1 = 1, int num2 = 2) {
+	return num1 + num2;
+}
+int main() {
+	cout << Adder() << endl;	// 입력값 두개 호출
+	cout << Adder(5) << endl;	// 앞에 입력값이 바뀌고, 뒤에 입력값은 그대로
+	cout << Adder(3,5) << endl; // 둘 다 바뀜
+	return 0;
+}
+*/
---
+++ day03/Project1/Project1/240222.default2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int Adder(int num1 = 1, int num2 = 2);	// 함수 선언
+int main() // 함수 호출
+{
+	cout << Adder() << endl;
+	cout << Adder(5) << endl;
+	cout << Adder(3,5) << endl;
+	return 0;
+
+}
+int Adder(int num1, int num2)	// 함수 정의
+{
+	return num1 + num2;
+}
+// 클래스 안에 들어가는 함수 메서드라 부름 *****
