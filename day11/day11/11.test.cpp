73918bf7e3394ea33bcbe80eb86f20b20c66042f
yongseok2312 <135982451+yongseok2312@users.noreply.github.com>
2024-03-10 15:25:01 +0900

    Update 11.test.cpp

--- day11/day11/11.test.cpp
+++ day11/day11/11.test.cpp
@@ -25,19 +25,26 @@ public:
 	friend Grade operator+ (const Grade& n1, const Grade& n2);
 };
 
-Grade operator+(const Grade &t, const Grade &q)
-{
-	Grade x(q.name, t.kr + q.kr, t.math + q.math, t.english + q.english);
+Grade operator+(const Grade& t, const Grade& q) {
+	Grade x;
+	x.name = "Total"; // Combine names manually if necessary
+	x.kr = t.kr + q.kr;
+	x.math = t.math + q.math;
+	x.english = t.english + q.english;
 	return x;
 	
 }
 
 int main() {
-	//for (int i=0;i<=5;i++)
+	const int numStudent = 5;
+	Grade students[numStudent];
+	for (int i = 0; i < numStudent; i++)
+	{
+		cout << " 학생" << i + 1 << " 의 정보를 입력하세요" << endl;
+		students[i]=Grade();
+	}
 		
-	Grade a;
-	Grade b;
-	Grade total = a + b;
+	
 	//cout << "국어" << total.kr << "수학" << total.math << "영어" << total.english << "입니다." << endl;
 	
 	return 0;
